50e231ea95c8e58676e4c10d8d87f1550c7b3c31
tshadz <thais.sombra@hotmail.com>
2024-07-20 13:05:00 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AppointmentsModule } from './appointments/appointments.module';
+
+@Module({
+  imports: [AppointmentsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
